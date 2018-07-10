45a8aa4464dd669c3ebb1668fd7af0326812a07b
Preetham D <preethamdbangera@gmail.com>
2018-07-10 11:23:02 +0530

    English to braille

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
  * in english language to that in the equivalent braille
  * code.
  */
+
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+function convert() {
+  const slang = document.getElementById('sourceLangText').value;
+  const mapping = new Map(englishToBrailleLiteralSet);
+  // console.log(mapping);
+  let out = '';
+  let i;
+  for (i = 0; i < slang.length; i += 1) {
+    out += mapping.get(slang[i]);
+  }
+  // console.log(out);
+  document.getElementById('targetLangText').innerHTML = out;
+}
+document.getElementById('btnConvertEnglishToBraille').addEventListener('click', convert);
